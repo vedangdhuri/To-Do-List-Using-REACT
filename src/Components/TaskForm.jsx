9ed61415b150af2e5f5b0ce848179641b761ab6e
Vedang Dhuri <netvedang@gmail.com>
2025-10-23 02:38:59 +0530

    commit 2

--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -3,38 +3,54 @@ import Tag from "./Tag.jsx";
 import "./TaskForm.css";
 
 const TaskForm = () => {
-  const [task, setTask] = useState("");
-  const [status, setStatus] = useState("todo")
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo"
+  })
+  const handleChange = (e) => {
+    const { name, value } = e.target.name
 
-  const handleTaskChange = (e) => {
-    setTask(e.target.value);
+    setTaskData(prev => {
+      return {
+        ...prev, [name]: value
+      }
+    })
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <header className="app_header">
-      <form>
+      <form onClick={handleSubmit}>
         <input
           type="text"
+          name="task"
           className="task_input"
           placeholder="Enter Your Task"
-          onChange={handleTaskChange}
+          onChange={handleChange}
         />
         <div className="task_form_button_line">
-          <Tag tagName="HTML" />
-          <Tag tagName="CSS" />
-          <Tag tagName="JavaScript" />
-          <Tag tagName="React" />
-          <Tag tagName="Python" />
-          <Tag tagName="Java" />
           <div>
-            <select className="task_status">
+            <Tag tagName="HTML" />
+            <Tag tagName="CSS" />
+            <Tag tagName="JavaScript" />
+            <Tag tagName="React" />
+            <Tag tagName="Python" />
+            <Tag tagName="Java" />
+          </div>
+
+          <div>
+            <select name="status" className="task_status" onChange={handleChange}>
               <option value="todo">To Do</option>
               <option value="doing">In Progress</option>
               <option value="done">Done</option>
             </select>
+            <button type="submit" className="task_submit">
+              + Add Task
+            </button>
           </div>
-          <button type="submit" className="task_submit">
-            + Add Task
-          </button>
         </div>
       </form>
     </header>
