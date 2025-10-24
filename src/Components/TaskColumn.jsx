388b5e8adc99fa72f8ce340d3395dfe910bab906
Vedang Dhuri <netvedang@gmail.com>
2025-10-24 22:29:42 +0530

    1th

--- src/Components/TaskColumn.jsx
+++ src/Components/TaskColumn.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./TaskColumn.css";
 import TaskCard from "./TaskCard.jsx";
-const TaskColumn = ({ icon, columnName }) => {
+const TaskColumn = ({ icon, columnName, tasks, status }) => {
   return (
     <section className="task_column">
       <h2 className="task_column_heading">
         <img className="task_column_icon" src={icon} alt="" /> {columnName}
       </h2>
 
-      <TaskCard />
+      {
+        tasks.map((task, index) => {
+          tasks.status === status &&
+            <TaskCard key={index} columnName={task.task} tags={task.tags} />
+        })
+      }
     </section>
   );
 };
