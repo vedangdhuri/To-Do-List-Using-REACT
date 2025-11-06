bb558a7a2618437e80674495b1e00000c4961ad2
Vedang Dhuri <netvedang@gmail.com>
2025-11-06 14:09:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
+
+const path = "./data.json";
+
+const markCommit = (x, y) => {
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = {
+    date: date,
+  };
+
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  });
+};
+
+const makeCommits = (n) => {
+  if (n === 0) return simpleGit().push();
+  const x = random.int(50, 53);
+  const y = random.int(0, 6);
+  const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+
+  const data = {
+    date: date,
+  };
+  console.log(date);
+  jsonfile.writeFile(path, data, () => {
+    simpleGit()
+      .add([path])
+      .commit(date, { "--date": date }, makeCommits.bind(this, --n));
+  });
+};
+
+makeCommits(20);
+// const n = 20;
+// for (let i = 0; i < n; i++) {
+//   markCommit(4, 4);
+// }
